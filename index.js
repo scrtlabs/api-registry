8bcf1922d3924107890c257c19986741e1b161cb
Adam Avramov <adam.b.avramov@gmail.com>
2022-01-13 00:45:07 +0100

    refactor: add structured data

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'holodeck-2':  require('./holodeck-2/holodeck-2.json'),
+  'pulsar-1':    require('./pulsar-1/pulsar-1.json'),
+  'pulsar-2':    require('./pulsar-2/pulsar-2.json'),
+  'supernova-2': require('./supernova-2/supernova-2.json')
+}
